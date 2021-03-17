3c718ecf55d0c1f48f039aa80add7c1b2c73d898
Dewiasmitalubis <mytaloebis@gmail.com>
2021-03-17 14:21:56 +0700

    Belajar mixin

---
+++ mixin.dart
@@ -0,0 +1,11 @@
+import 'package:pbo_dart_console/knight_hero.dart';
+import 'package:pbo_dart_console/monster.dart';
+import 'package:pbo_dart_console/monster_kecoa.dart';
+import 'package:pbo_dart_console/monster_ubur_ubur.dart';
+import 'package:pbo_dart_console/monster_ucoa.dart';
+import 'package:pbo_dart_console/swimming_monster.dart';
+
+void main(List<String> args) {
+  KnightHero knight = KnightHero();
+  print(knight.drink());
+}
